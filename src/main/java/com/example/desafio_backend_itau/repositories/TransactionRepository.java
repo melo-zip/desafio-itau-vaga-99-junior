c330c72c6eef1b2c9d0e66466eb25fdcb474f73d
melo-zip <matheus.webp@gmail.com>
2025-01-26 04:53:08 -0300

    feat: add TransactionRepository

---
+++ src/main/java/com/example/desafio_backend_itau/repositories/TransactionRepository.java
@@ -0,0 +1,25 @@
+package com.example.desafio_backend_itau.repositories;
+
+import com.example.desafio_backend_itau.models.TransactionModel;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class TransactionRepository {
+    List<TransactionModel> transactions = new ArrayList<>();
+
+    public List<TransactionModel> getTransactions() {
+        return transactions;
+    }
+
+    public TransactionModel addTransaction(TransactionModel transactionModel) {
+        transactions.add(transactionModel);
+        return transactionModel;
+    }
+
+    public void deleteTransactions(){
+        transactions.clear();
+    }
+}
