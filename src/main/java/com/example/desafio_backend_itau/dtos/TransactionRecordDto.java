a3ec5dab4dfe5b58dae1c99180da940906e9d55b
melo-zip <matheus.webp@gmail.com>
2025-01-26 06:35:25 -0300

    refactor: change TransactionRecordDto validation

--- src/main/java/com/example/desafio_backend_itau/dtos/TransactionRecordDto.java
+++ src/main/java/com/example/desafio_backend_itau/dtos/TransactionRecordDto.java
@@ -1,6 +1,8 @@
 package com.example.desafio_backend_itau.dtos;
 
+import jakarta.validation.constraints.NotNull;
+
 import java.time.OffsetDateTime;
 
-public record TransactionRecordDto(Double value, OffsetDateTime time) {
+public record TransactionRecordDto(@NotNull(message = "value field cannot be null") Double value,@NotNull(message = "time field cannot be null") OffsetDateTime time) {
 }
--- src/main/java/com/example/desafio_backend_itau/helpers/TransactionValidator.java
+++ src/main/java/com/example/desafio_backend_itau/helpers/TransactionValidator.java
@@ -6,6 +6,6 @@ import java.time.OffsetDateTime;
 
 public interface TransactionValidator {
     default boolean validate(TransactionModel transactionModel){
-        return transactionModel.getValue() != null && transactionModel.getValue() >= 0 && transactionModel.getTime() != null && transactionModel.getTime().isBefore(OffsetDateTime.now());
+        return transactionModel.getValue() >= 0  && transactionModel.getTime().isBefore(OffsetDateTime.now());
     }
 }
