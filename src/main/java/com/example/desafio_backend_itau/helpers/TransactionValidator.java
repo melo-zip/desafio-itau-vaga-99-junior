5db560c518d8ebd04182a411fac21453b39990cf
melo-zip <matheus.webp@gmail.com>
2025-01-28 10:20:33 -0300

    refactor: change TransactionValidator

--- src/main/java/com/example/desafio_backend_itau/helpers/TransactionValidator.java
+++ src/main/java/com/example/desafio_backend_itau/helpers/TransactionValidator.java
@@ -3,9 +3,20 @@ package com.example.desafio_backend_itau.helpers;
 import com.example.desafio_backend_itau.models.TransactionModel;
 
 import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public interface TransactionValidator {
-    default boolean validate(TransactionModel transactionModel){
-        return transactionModel.getValue() >= 0  && transactionModel.getTime().isBefore(OffsetDateTime.now());
+    default List<String> validate(TransactionModel transactionModel){
+        List<String> errors = new ArrayList<>();
+
+        if (transactionModel.getValue() < 0) {
+            errors.add("Transaction value cannot be negative.");
+        }
+        if (transactionModel.getTime().isAfter(OffsetDateTime.now())) {
+            errors.add("Transaction time cannot be in the future.");
+        }
+
+        return errors;
     }
 }
